fc63ac6d6a55d218d811300bc67df43464bb208c
Maggie <muriithinyawira13@gmail.com>
2022-11-28 09:30:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+var superbowlWin= (gameList)=>{
+    const winYear = gameList.find(win => win.result === "W");
+
+        if(winYear){
+            return winYear.year;
+       } else {
+            return undefined;
+    }
+}
+
+
